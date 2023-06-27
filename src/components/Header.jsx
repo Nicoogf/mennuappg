00e81b40ad59c35a99b4745d0f29fe67990b8b3e
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-27 20:15:16 -0300

    Se agrega animacion al menu desplegable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,9 @@ const Header = () => {
   const firebaseAuth = getAuth(app) ;
   const provider = new GoogleAuthProvider();
 
-  const [{user} , dispatch ] = useStateValue()
+  const [{user} , dispatch ] = useStateValue() ;
+
+  const [isMenu , setIsMenu] = useState(false) ;
 
   const login = async() =>{
 
@@ -32,6 +34,8 @@ const Header = () => {
     
         })
         localStorage.setItem("user" , JSON.stringify(providerData[0]))
+      }else{
+        setIsMenu(!isMenu)
       }
   };
 
@@ -87,7 +91,12 @@ const Header = () => {
             onClick={login} 
             />
 
-            <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 '>
+            {
+              isMenu && (
+                <motion.div initial={{opacity : 0 , scale: 0.6}}
+                            animate={{opacity : 1 , scale: 1}}
+                            exit={{opacity : 0 , scale: 0.6}}
+                            className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 '>
             
             {
               user && user.email === "nicolasgfalabella@gmail.com" && ( 
@@ -104,7 +113,9 @@ const Header = () => {
             
             <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Cerrar Seccion <MdLogout /></p>
 
-            </div>
+            </motion.div>
+              )
+            }
 
           </div>
 
