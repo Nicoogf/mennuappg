2d385b1e66a9d49548a20ceba5bb514ef2634c5d
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-27 20:04:14 -0300

    Se agregan las opciones para el user si esta logueado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react' ;
-import {MdShoppingBasket} from "react-icons/md" ;
+import { MdShoppingBasket , MdAdd , MdLogout } from "react-icons/md" ;
 import { motion } from "framer-motion" ;
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -87,10 +87,22 @@ const Header = () => {
             onClick={login} 
             />
 
-            <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 px-4 py-2'>
+            <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 '>
+            
+            {
+              user && user.email === "nicolasgfalabella@gmail.com" && ( 
 
-            <p>Agregar Producto</p>
-            <p>Cerrar Seccion</p>
+                <Link to={"/createItem"}>
+
+                 <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Agregar Producto <MdAdd />
+                 </p>
+
+                </Link>
+              )
+            }
+
+            
+            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Cerrar Seccion <MdLogout /></p>
 
             </div>
 
