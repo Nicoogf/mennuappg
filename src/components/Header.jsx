e914c65cc8e2064aca5b02fd86b9c2bbb3e7ae34
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-27 22:16:58 -0300

    Se trabaja sobre el header mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
 
   return (
 
-    <header className='fixed z-50 w-screen p-6 px-16'>
+    <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
 
        { /* Escritorio y Tablet */ }
 
@@ -54,7 +54,10 @@ const Header = () => {
 
         <div className='flex items-center gap-8'>
 
-          <ul className='flex items-center gap-8 ml-auto'>
+          <motion.ul initial={{opacity: 0 , x : 200}}
+                     animate={{opacity: 1 , x : 0}}
+                     exit={{opacity: 0 , x : 200}}
+                     className='flex items-center gap-8 ml-auto'>
 
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'> 
             Home 
@@ -72,7 +75,7 @@ const Header = () => {
             Servicios
             </li>
 
-         </ul>
+         </motion.ul>
 
          <div className='relative flex items-center justify-center'>
 
@@ -127,8 +130,49 @@ const Header = () => {
       { /* Mobile */ }
 
 
-      <div className='flex md:hidden w-full h-full  '>
+      <div className='flex items-center justify-between md:hidden w-full h-full  '>
 
+      <Link to={"/"}className='flex items-center gap-2'>
+              <img src={Logo} className="w-8 object-cover" alt="Logo de Pagina"/>
+              <p className='text-headingColor text-xl font-bold'>Algo Dulce</p>
+        </Link>
+
+        <div className='relative'>
+
+          <motion.img whileTap={{scale : 0.6}}
+            src={user ? user.photoURL : Avatar} 
+            className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full' 
+            alt="imagen de perfil"
+            onClick={login} 
+            />
+
+            {
+              isMenu && (
+                <motion.div initial={{opacity : 0 , scale: 0.6}}
+                            animate={{opacity : 1 , scale: 1}}
+                            exit={{opacity : 0 , scale: 0.6}}
+                            className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 '>
+            
+            {
+              user && user.email === "nicolasgfalabella@gmail.com" && ( 
+
+                <Link to={"/createItem"}>
+
+                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Agregar Producto <MdAdd />
+                </p>
+
+                </Link>
+              )
+            }
+
+            
+            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Cerrar Seccion <MdLogout /></p>
+
+            </motion.div>
+    )
+  }
+
+        </div>
 
       </div>
 
