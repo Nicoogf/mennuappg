ce972ed40d607a521578743a5462d0fb44781366
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-29 08:35:37 -0300

    Se agrega titulo de prueba al mainContainer

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Delivery from "../img/delivery.png"
 
 const MainContainer = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
 
     <div className='py-2  flex-1 flex flex-col items-start md:items-center justify-center'>
-      <div>
-        <p>
-          Titulo de la pagina
-        </p>
+      <div className='flex items-center gap-2 justify-center bg-orange-100 p-2 rounded-full'>
+        <p className='text-base text-orange-500 font-semibold'> Titulo de la pagina </p>
+
+        <div className='w-6 h-6 bg-white rounded-full overflow-hidden'>
+            <img src={Delivery} className='w-full h-full object-contain'  alt="Delivery" />
+        </div>    
+
       </div>
     </div>
     <div className='py-2  flex-1'></div>
