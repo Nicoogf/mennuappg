8157537f668782da41d4b8d94704a5239a26fe03
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-28 21:08:04 -0300

    Se setea el boton de logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,6 +39,16 @@ const Header = () => {
       }
   };
 
+    const logout = () =>{
+      setIsMenu(false)
+      localStorage.clear()
+
+      dispatch({
+        type : actionType.SET_USER,
+        user : null
+      });
+    };
+
   return (
 
     <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
@@ -95,8 +105,8 @@ const Header = () => {
             />
 
             {
-              isMenu && (
-                <motion.div initial={{opacity : 0 , scale: 0.6}}
+            isMenu && (
+            <motion.div initial={{opacity : 0 , scale: 0.6}}
                             animate={{opacity : 1 , scale: 1}}
                             exit={{opacity : 0 , scale: 0.6}}
                             className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 '>
@@ -110,15 +120,14 @@ const Header = () => {
                  </p>
 
                 </Link>
-              )
-            }
+              )}
 
-            
-            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Cerrar Seccion <MdLogout /></p>
 
-            </motion.div>
-              )
-            }
+                        
+              <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick={logout}> Salir <MdLogout /></p>
+
+              </motion.div>
+              )}
 
           </div>
 
@@ -162,11 +171,31 @@ const Header = () => {
                 </p>
 
                 </Link>
-              )
-            }
+              )}
+
+          <ul className='flex flex-col'>
+
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'> 
+            Home 
+            </li>
+
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'> 
+            Menu 
+            </li>
+
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'> 
+            About Us 
+            </li>
+
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>
+            Servicios
+            </li>
+
+         </ul>
 
             
-            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Cerrar Seccion <MdLogout /></p>
+            <p className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base'
+              onClick={logout}> Salir <MdLogout /></p>
 
             </motion.div>
     )
