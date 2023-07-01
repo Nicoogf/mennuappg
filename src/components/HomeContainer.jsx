3fef6ff871809ba03f81cee01dd86db0d4754138
Nicoogf <nicolasgfalabella@gmail.com>
2023-07-01 11:29:45 -0300

    Cambios del HomeContainer

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -33,12 +33,18 @@ const HomeContainer = () => {
     
 
 
-    <div className='py-2 flex-1 flex items-center'>
+    <div className='py-2 flex-1 flex items-center relative'>
 
        
             <img src={HeroBg}  className="ml-auto h-400 w-full lg:w-auto lg:h-650" alt="Coockies" />
 
-            <div className='w-full h-full absolute flex items-center justify-center'></div>
+            <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4'>
+
+              <div className='w-190 p-2 bg-cardOverlay backdrop-blur-md'>
+
+              </div>
+
+            </div>
        
        
     </div>
