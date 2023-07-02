68132fc26af270cea6a9e93b5784cec2678912c4
Nicoogf <nicolasgfalabella@gmail.com>
2023-07-02 19:44:13 -0300

    Se reubican los productos

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -41,18 +41,19 @@ const HomeContainer = () => {
        
             <img src={HeroBg}  className="ml-auto h-400 w-full lg:w-auto lg:h-650" alt="Coockies" />
 
-            <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4 gap-4 flex-wrap'>
+            <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center py-4 gap-4 flex-wrap'>
 
               
 
             {heroData && heroData.map( n => (
 
-              <div key = {n.id} className='w-190 min-w-[190px] p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex items-center justify-center flex-col drop-shadow-lg'>
-              <img src={n.imgScr} className="w-40 -mt-20" alt="" />
+              <div key = {n.id} className='lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center  drop-shadow-lg'>
 
-              <p className='text-xl font-semibold text-textColor mt-4'>{n.name}</p>
+              <img src={n.imgScr} className="w-20 lg:w-40 -mt-10 lg:-mt-20" alt="" />
 
-              <p className='text-sm text-gray-500 font-semibold my-3'> {n.desc} </p>
+              <p className='text-base text-xl font-semibold text-textColor mt-2 lg:mt-4'>{n.name}</p>
+
+              <p className='text-[12px] lg:text-sm text-gray-500 font-semibold my-1 lg:my-3'> {n.desc} </p>
 
               <p className='text-sm font-semibold text-headingColor'> <span className='text-xs text-red-600'>$</span> {n.price} </p>
 
