9cd43dbc3aa9d6f94ef6c6d618cdd2894f5a48c2
Nicoogf <nicolasgfalabella@gmail.com>
2023-07-02 21:26:00 -0300

    Se inicia el createConteiner

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateContainer = () => {
+  const [title, setTitle] = useState ("") ;
+  const [calories, setCalories] = useState ("") ;
+  const [price, setPrice] = useState ("") ;
+  const [category, setCategory] = useState (null) ;
+  const [imageAsset , setImageAsset] = useState(null) ; 
+  const [fields, setFields] = useState (true) ;
+  const [alertStatus , steAlertStatus] = useState("s") ; 
+  const [msg , setMsg ] = useState(null) ;
+  const [ isLoading, setIsLoading] = useState(false);
+
+
+
   return (
-    <div>CreateContainer</div>
+    <div classname="w-full min-h-screen flex items-center justify-center">
+ 
+        <div classname ="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center">
+          {
+            fields && (
+              <p className={`w-full p-2 rounded-lg text-center text-lg font-semibold  ${ 
+                alertStatus === "danger"
+                ? 'bg-red-400 text-red-800'
+                : "bg-emerald-400 text-emerald-800"
+                }`}
+              > 
+
+                Something Wrong
+              </p>
+            )
+          }
+        </div>
+
+    </div>
   )
 }
 
-export default CreateContainer
+export default CreateContainer ;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -116,7 +116,7 @@ const Header = () => {
 
                 <Link to={"/createItem"}>
 
-                 <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Agregar Producto <MdAdd />
+                 <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick = { () => setIsMenu(false)}> Agregar Producto <MdAdd />
                  </p>
 
                 </Link>
@@ -178,7 +178,8 @@ const Header = () => {
 
                 <Link to={"/createItem"}>
 
-                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Agregar Producto <MdAdd />
+                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                onClick = { () => setIsMenu(false)} > Agregar Producto <MdAdd />
                 </p>
 
                 </Link>
@@ -186,19 +187,19 @@ const Header = () => {
 
           <ul className='flex flex-col'>
 
-            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'> 
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'  onClick = { () => setIsMenu(false)}> 
             Home 
             </li>
 
-            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'> 
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick = { () => setIsMenu(false)}> 
             Menu 
             </li>
 
-            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'> 
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick = { () => setIsMenu(false)}> 
             About Us 
             </li>
 
-            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick = { () => setIsMenu(false)}>
             Servicios
             </li>
 
